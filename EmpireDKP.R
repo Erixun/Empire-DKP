5ba7e135bdb7786fff24907a096468550f4ef987
Erik Sundberg <erik.sund.berg20@hotmail.se>
2019-12-22 18:12:19 +0100

    Centered headings,  added Ragnaros

--- EmpireDKP.R
+++ EmpireDKP.R
@@ -1,9 +1,5 @@
 
 library(shiny)
-#library(shinyWidgets)
-#library(dplyr)
-#library(xml2)
-#library(rvest)
 library(readr)
 library(DT)
 
@@ -11,30 +7,41 @@ df_dkp <- as.data.frame(read_csv("dkp.csv"))
 
 ui <- fluidPage( 
   
-  titlePanel(
-    div(h6("DKP status for members of the guild",img(src="https://gamepedia.cursecdn.com/wowpedia/6/60/AllianceLogo.png?version=5a03696dc7259ecc5cc6d97ec98ee8ea", height=80, width=70, style={'margin-right:10px;float:left;'}), style={'margin:0px; padding-bottom:0px;padding-top:5px;'}), 
-        h1("Empire", style={'font-family:copperplate; font-size:50px; -webkit-text-stroke-width: 1px; -webkit-text-stroke-color: black;
+  
+  column(6, 
+         titlePanel(
+    div(
+      h6("DKP status for members of", align="center",
+         img(src="AllianceLogo.png", height=80, width=70, style={'margin-right:10px;float:left;'}),
+         img(src="AllianceLogo.png", height=80, width=70, style={'margin-right:10px;float:right;'}), 
+         style={'margin:0px;float:center; padding-bottom:0px;padding-top:5px;'}), 
+        h1("Empire", align="center", 
+         style={'font-family:copperplate; font-size:50px; -webkit-text-stroke-width: 1px; -webkit-text-stroke-color: black;
     -webkit-font-smoothing: antialiased; color:darkred; margin-top:0px; margin-bottom:0px; padding-top:0px;'}), 
-        em(h6("Bloodfang EU", style={'margin-top:0px;'})), 
-        style={'height:90px; width:49%; padding-left:10px; background-color:lightblue;'}
+        em(h6("Bloodfang EU", align="center", 
+              style={'margin-top:0px;'})), align="center",
+        style={'height:92px; width:100%; padding-left:10px; background-color:lightblue;'}
         ),
     windowTitle = "Empire DKP"
   ),
   br(),
       fluidRow(
-        column(6, 
                dataTableOutput("dtbl")
-        )
       )
-
+  ),
+  column(6,
+         div(img(src="Ragnaros.jpg", height="980px", width="630px", style={"margin-top:20px;"}) ) 
+         )
 )
 
 server <- function(input,output) {
   
   output$dtbl <- renderDataTable(
     options = list(
-      pageLength = 50
-    ),
+      paging = TRUE,
+      pageLength = 20,
+      searching = TRUE
+      ),
     rownames = FALSE,
     df_dkp
   )
