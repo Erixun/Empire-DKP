c8c9e92e6147187084ab8b5573c907c279c6c94b
Erik Sundberg <erik.sund.berg20@hotmail.se>
2020-01-02 18:20:12 +0100

    Update 02-01-2020

--- Data extraction.R
+++ Data extraction.R
@@ -1,14 +1,15 @@
 
-#setwd("~/Documents/R stuff/Empire-DKP")
+setwd("~/Documents/R stuff/Empire-DKP")
 
 library(dplyr)
 library(xml2)
 library(rvest)
 library(readr)
 
+# DKP table
 rel_nodes <- read_html("dkp.html", encoding = "UTF-8") %>% 
   html_nodes("div#DKP.tab") %>% 
-  html_nodes("div#DKP1.divTableBody") %>% 
+  html_nodes("div.divTableBody") %>% 
   html_nodes("div.divTableRow")
 Player <- rel_nodes %>% 
   html_nodes("div.divPlayer") %>% 
@@ -32,7 +33,28 @@ class_text <- rel_nodes %>%
 Class <- class_text[seq(1,length(class_text), by=2)] %>%
   sapply(.simpleCap) %>% unname()
 
+#Player[grep("Britn", Player)] <- "Britnéyfears"
 
 data.frame(Player, Class, DKP) %>% 
   write_csv("dkp.csv")
 
+# Loot history
+# rel_nodes2 <- read_html("dkp.html", encoding = "UTF-8") %>% 
+#   html_nodes("div#LootHistory.tab") %>% 
+#   html_nodes("div.divTableBody") %>% 
+#   html_nodes("div.divTableRow")
+# Player <- rel_nodes2 %>% 
+#   html_nodes("div.divClass") %>% 
+#   html_text()
+# DKPchange <- rel_nodes2 %>% 
+#   html_nodes("div.divPlayer") %>%
+#   #html_nodes("a") %>%
+#   as.character()
+# # %>% 
+#   #html_text()
+# RaidBoss <- rel_nodes2 %>% 
+#   html_nodes("div.divDKP") %>% 
+#   html_text()
+# as.character(read_html("dkp copy.html"))
+# read_html(DKPchange)
+# DT::datatable(data.frame(DKPchange), escape=F)
--- EmpireDKP.R
+++ EmpireDKP.R
@@ -5,32 +5,37 @@ library(DT)
 
 df_dkp <- as.data.frame(read_csv("dkp.csv"))
 
-ui <- fluidPage( 
-  
-  
+ui <- fluidPage(id="page",
+                
+                tags$style(type = "text/css", "h2 {margin: 0; !important;}"),
+                tags$style(type = "text/css", "div.col-sm-6 {margin: 0; padding-left: 0px; padding-right: 0px; !important;}"),
+                tags$style(type = "text/css", "div#page.container-fluid::before {display: none; !important;}"),
+                tags$style(type = "text/css", "div#page.container-fluid {margin: 0; padding: 0; !important;}"),
+                tags$style(type = "text/css", "div.row {margin: 0px 10px 0px 10px; !important;}"),
   column(6, 
          titlePanel(
     div(
       h6("DKP status for members of", align="center",
-         img(src="AllianceLogo.png", height=80, width=70, style={'margin-right:10px;float:left;'}),
-         img(src="AllianceLogo.png", height=80, width=70, style={'margin-right:10px;float:right;'}), 
+         img(src="Alliance_icon.svg", height=88, width=80, style={'margin-left: 20px;float:left;'}),
+         img(src="Alliance_icon.svg", height=88, width=80, style={'margin-right: 20px;float:right;'}), 
          style={'margin:0px;float:center; padding-bottom:0px;padding-top:5px;'}), 
         h1("Empire", align="center", 
          style={'font-family:copperplate; font-size:50px; -webkit-text-stroke-width: 1px; -webkit-text-stroke-color: black;
     -webkit-font-smoothing: antialiased; color:darkred; margin-top:0px; margin-bottom:0px; padding-top:0px;'}), 
         em(h6("Bloodfang EU", align="center", 
-              style={'margin-top:0px;'})), align="center",
-        style={'height:92px; width:100%; padding-left:10px; background-color:lightblue;'}
+              style={'margin-top:0px; margin-bottom: 5px;'})), #align="center",
+        style={'height: 95px; width: auto; margin-top: 10px; margin-left: 10px; margin-right: 10px; padding: 0; background-color:lightblue;'}
         ),
     windowTitle = "Empire DKP"
   ),
   br(),
       fluidRow(
                dataTableOutput("dtbl")
-      )
+      ),
+  style={"margin-top: 0px;"}
   ),
   column(6,
-         div(img(src="Ragnaros.jpg", height="980px", width="630px", style={"margin-top:20px;"}) ) 
+         div(img(src="Ragnaros.jpg", height="980px", width="640px", style={"margin-top:0px;"}) ) 
          )
 )
 
