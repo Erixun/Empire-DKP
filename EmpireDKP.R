2f6ca9f17a835caec32b24bc766682e0d9892d9b
Erik Sundberg <erik.sund.berg20@hotmail.se>
2020-01-14 22:34:57 +0100

    Made background image scale with zooming in/out in the browser

--- EmpireDKP.R
+++ EmpireDKP.R
@@ -1,4 +1,6 @@
 
+#setwd("~/Documents/R stuff/empire-dkp")
+
 library(shiny)
 library(readr)
 library(DT)
@@ -7,37 +9,41 @@ df_dkp <- as.data.frame(read_csv("dkp.csv"))
 
 ui <- fluidPage(id="page",
                 
-                tags$style(type = "text/css", "h2 {margin: 0; !important;}",
-                "div#col1 {margin: 0; border: 4px; border-style: solid; border-color: black; padding-left: 0px; padding-right: 0px; !important;}",
-                "div#page.container-fluid::before {display: none; !important;}",
-                "div#page.container-fluid {margin: 0; padding: 0; !important;}",
-                "div.row {margin: 0px 10px 0px 10px; !important;}",
-                "#DataTables_Table_0_filter {float: left;}"),
-  column(6, id = "col1",
-         titlePanel(
+  tags$style(type = "text/css", "h2 {margin: 0; !important;}",
+  "div#col1 {margin: 0; border: 4px; border-style: solid; border-color: black; padding-left: 0px; padding-right: 0px; !important;}",
+  "div#page.container-fluid::before {display: none; !important;}",
+  "div#page.container-fluid {margin: 0; padding: 0; !important;}",
+  "div.row {margin: 0px 10px 0px 10px; !important;}",
+  "#DataTables_Table_0_filter {float: left;}"),
+  
+  column(6, id = "col1", #left column w table
+    titlePanel(
+      div(
+        h6("DKP status for members of", align="center",
+           img(src="Alliance_icon.svg", height=88, width=80, style={'margin-left: 20px;float:left;'}),
+           img(src="Alliance_icon.svg", height=88, width=80, style={'margin-right: 20px;float:right;'}), 
+           style={'margin:0px;float:center; padding-bottom:0px;padding-top:5px;'}), 
+          h1("Empire", align="center", 
+           style={'font-family:copperplate; font-size:70px; -webkit-text-stroke-width: 1px; -webkit-text-stroke-color: black;
+      -webkit-font-smoothing: antialiased; color:darkred; margin-top:-10px; margin-bottom:-10px; padding-top:0px;'}), 
+          em(h6("Bloodfang EU", align="center", 
+                style={'margin-top:0px; margin-bottom: 5px;'})), #align="center",
+          style={'height: 95px; width: auto; margin-top: 10px; margin-left: 10px; margin-right: 10px; padding: 0; background-color:lightblue;'}
+          ),
+      windowTitle = "Empire DKP"
+    ),
+    br(),
+    fluidRow(
+      dataTableOutput("dtbl")
+    ),
+    style={"margin-top: 0px;"}
+  ),
+  
+  column(6, #right column w bgimage
     div(
-      h6("DKP status for members of", align="center",
-         img(src="Alliance_icon.svg", height=88, width=80, style={'margin-left: 20px;float:left;'}),
-         img(src="Alliance_icon.svg", height=88, width=80, style={'margin-right: 20px;float:right;'}), 
-         style={'margin:0px;float:center; padding-bottom:0px;padding-top:5px;'}), 
-        h1("Empire", align="center", 
-         style={'font-family:copperplate; font-size:50px; -webkit-text-stroke-width: 1px; -webkit-text-stroke-color: black;
-    -webkit-font-smoothing: antialiased; color:darkred; margin-top:0px; margin-bottom:0px; padding-top:0px;'}), 
-        em(h6("Bloodfang EU", align="center", 
-              style={'margin-top:0px; margin-bottom: 5px;'})), #align="center",
-        style={'height: 95px; width: auto; margin-top: 10px; margin-left: 10px; margin-right: 10px; padding: 0; background-color:lightblue;'}
-        ),
-    windowTitle = "Empire DKP"
-  ),
-  br(),
-      fluidRow(
-               dataTableOutput("dtbl")
-      ),
-  style={"margin-top: 0px;"}
-  ),
-  column(6,
-         div(img(src="Ragnaros_original2.jpg", height="640px", width="750px", style={"margin-top:0px; margin-left: -15px; position: fixed;"}) ) 
-         )
+      img(src="Ragnaros_original2.jpg", height="100%", width="50%", style={"margin-top:0px; margin-left: -15px; position: fixed;"}) 
+       ) 
+      )
 )
 
 server <- function(input,output) {
